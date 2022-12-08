50216bcca1d2d47f59b89a1a8615d4d93948c726
nwogburumichael~ <michael.nwogburu.junior@gmail.com>
2022-12-08 15:33:33 +0100

    Ajout des dossier pour le projet

---
+++ Programme/Projet_S/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <WifiClient.h>
+#include <PubSubClient.h>
+#include <Wifi.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <ArduinoJson.h>
+#include <Firebase_ESP_Client.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
